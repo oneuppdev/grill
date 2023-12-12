473d6755bf0f1d7b760e3bd603ae20d2ae29a9f2
Henry Berson Saint-Juste <oneuppdev@gmail.com>
2023-12-11 21:23:04 -0500

    feat: implement color mode toggle and update icons arcordingly

--- apps/web/src/components/navigationBar/NavigationBar.tsx
+++ apps/web/src/components/navigationBar/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   HStack,
   List,
@@ -6,12 +7,20 @@ import {
   Button,
   Show,
   Image,
+  useColorMode,
 } from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 import { PiShoppingCartBold } from 'react-icons/pi';
 import { IoMdMenu } from 'react-icons/io';
 import logo from '@assets/the-island-express-grill.png';
 
 const NavigationBar = (): JSX.Element => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const isDarkMode = React.useMemo(() => {
+    return colorMode === 'dark';
+  }, [colorMode]);
+
   return (
     <HStack
       justifyContent={{
@@ -40,6 +49,13 @@ const NavigationBar = (): JSX.Element => {
           boxSize="50px"
           borderRadius="25px"
           icon={<PiShoppingCartBold />}
+        />
+        <IconButton
+          aria-label="Color Mode"
+          boxSize="50px"
+          borderRadius="25px"
+          icon={isDarkMode ? <MoonIcon /> : <SunIcon />}
+          onClick={toggleColorMode}
         />
         <Show below="lg">
           <IconButton
